a0524fdb43189f8d4003809d1fe460b800a7c558
Yansen <yansen626@gmail.com>
2019-11-29 10:11:12 +0700

    29 nov - edit minor

--- resources/views/layouts/frontend-v2.blade.php
+++ resources/views/layouts/frontend-v2.blade.php
@@ -5,6 +5,8 @@
     @yield('head_and_title')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!--===============================================================================================-->
